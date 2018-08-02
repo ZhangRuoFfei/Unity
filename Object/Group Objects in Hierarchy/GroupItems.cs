61056a3957fb86a5c6ad1886fbd7da43157b0678
lutos荷叶生 <921306701@qq.com>
2018-08-02 14:29:43 +0800

    Create GroupItems.cs

---
+++ Object/Group Objects in Hierarchy/GroupItems.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+
+public static class GroupItems
+{
+    [MenuItem("GameObject/Group Selected %g")]
+    private static void GroupSelected()
+    {
+        if (!Selection.activeTransform) return;
+
+        var go = new GameObject(Selection.activeTransform.name + " Group");
+        Undo.RegisterCreatedObjectUndo(go, "Group Selected");
+        go.transform.SetParent(Selection.activeTransform.parent, false);
+
+        foreach (var transform in Selection.transforms) Undo.SetTransformParent(transform, go.transform, "Group Selected");
+
+        Selection.activeGameObject = go;
+        
+    }
+
+    [MenuItem("GameObject/Group Selected %g", true)]
+    private static bool EnableGroupSelected()
+    {
+        if (!EditorApplication.isPlaying) return true;
+        //Debug.Log("Can't group in playing mode!");
+        return false;
+    }
+
+    [MenuItem("GameObject/Cancel Group Selected %#g")]
+    private static void CancelGroupSelected()
+    {
+        //select now
+        GameObject[] currentGroups = Selection.gameObjects;
+        //after cancel group in select
+        List<GameObject> AfterGroups = new List<GameObject>();
+
+        for(int m=0;m<currentGroups.Length;m++)
+        {
+            int count = currentGroups[m].transform.childCount;
+            for (int n=0;n<count;n++)
+            {
+                Transform temp = currentGroups[m].transform.GetChild(0);
+                AfterGroups.Add(temp.gameObject);
+                temp.SetParent(null);
+            }
+
+            //delete when it is an empty object
+            var components = currentGroups[m].GetComponents<Component>();
+            if (components.Length<=1&& currentGroups[m].transform.childCount == 0)
+                GameObject.DestroyImmediate(currentGroups[m]);
+        }
+        Selection.objects = AfterGroups.ToArray();
+
+    }
+
+    [MenuItem("GameObject/Cancel Group Selected %#g", true)]
+    private static bool EnableCancelGroupSelected()
+    {
+        if (!EditorApplication.isPlaying) return true;
+        //Debug.Log("Can't cancel group in playing mode!");
+        return false;
+    }
+
+}
